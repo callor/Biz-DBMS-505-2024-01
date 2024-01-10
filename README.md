dc211dc7d786d0c0870169c5c1f9ca7659655d9e
callor <callor@callor.com>
2024-01-10 13:39:15 +0900

    DBMS

---
+++ README.md
@@ -0,0 +1,4 @@
+# DBMS 프로젝트
+* MySQL 을 사용한 DBMS
+* MySQL 8.0.x DBMS Server 설치 : mysql.com 에서 community version 을 다운로드 받아 설치한다.
+
---
+++ root(2024-01-09-Start).sql
@@ -0,0 +1,18 @@
+/* 
+여러줄 주석 주석문  
+*/
+-- 한줄짜리 주석
+
+/*
+이 화면에서 SQL 명령어를 입력하고
+Ctrl + Enter 를 입력하면 명령이 실행된다
+명령어는 대체로 대소문자 구문이 없다
+주요 키워드는 대문자로, 변수 등은 소문자로 입력
+*/
+SHOW DATABASES;
+SHOW DATABASES;
+USE mysql;
+SHOW TABLES;
+SELECT * FROM user;
+
+
